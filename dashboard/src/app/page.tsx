dc57fc96a60bdbb68b01f49731f9627c6ad4924f
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-10-02 22:24:42 +0500

    setup

---
+++ dashboard/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="flex justify-center items-center h-screen underline decoration-double text-4xl text-gray-600">DashBoard</h1>
+    </>
+  );
+}
